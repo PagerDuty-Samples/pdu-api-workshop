9af94d23c5e6e0983ec68e1732d1130309165f86
Scott McAllister <smcallister@pagerduty.com>
2021-05-20 12:08:48 -0700

    update config vars

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const APP_CONFIG = {
-    redirectUrl: `https://0bf12e50cf32.ngrok.io`,
-    clientId: `30fd28a095c211eed9b0d4ea1fb1a3b24daf23a16d43beaefbcc1177f6679224`
+    redirectUrl: `https://pagerduty-samples.github.io/pdu-api-workshop/`,
+    clientId: `a68ad108ebfd5014565a06d815a6e85f1ff03cb2dd6f0b308439ac8b0c1b0759`
 };
